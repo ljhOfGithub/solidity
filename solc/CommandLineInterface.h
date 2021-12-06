1648222653185a56e53f0417efdde579987a4732
ljhOfGithub <42089575+ljhOfGithub@users.noreply.github.com>
2021-12-06 16:36:56 +0800

    annotations

--- solc/CommandLineInterface.h
+++ solc/CommandLineInterface.h
@@ -52,10 +52,13 @@ public:
 	{}
 
 	/// Parse command line arguments and return false if we should not continue
+	// 解析命令行参数，如果不应该继续则返回false
 	bool parseArguments(int _argc, char const* const* _argv);
 	/// Read the content of all input files and initialize the file reader.
+	// ///读取所有输入文件的内容并初始化文件读取器。
 	bool readInputFiles();
 	/// Parse the files, create source code objects, print the output.
+	// 解析文件，创建源代码对象，打印输出。
 	bool processInput();
 
 	CommandLineOptions const& options() const { return m_options; }
@@ -96,24 +99,27 @@ private:
 	/// such that they can be imported into the compiler  (importASTs())
 	/// (produced by --combined-json ast <file.sol>
 	/// or standard-json output
+	//尝试读取@ m_sourceCodes作为保存ast的json，这样它们可以被导入编译器(importsts())(由——combined-json ast <file产生)。< span="">Sol >或标准json输出</file产生)。<>
 	std::map<std::string, Json::Value> parseAstFromInput();
 
-	/// Create a file in the given directory
+	/// Create a file in the given directory///在指定的目录下创建一个文件
 	/// @arg _fileName the name of the file
 	/// @arg _data to be written
 	void createFile(std::string const& _fileName, std::string const& _data);
 
-	/// Create a json file in the given directory
+	/// Create a json file in the given directory///在给定的目录中创建一个json文件
 	/// @arg _fileName the name of the file (the extension will be replaced with .json)
 	/// @arg _json json string to be written
 	void createJson(std::string const& _fileName, std::string const& _json);
 
 	/// Returns the stream that should receive normal output. Sets m_hasOutput to true if the
 	/// stream has ever been used unless @arg _markAsUsed is set to false.
+	// 返回应该接收正常输出的流。设置m_hasOutput为true，如果流曾经被使用过，除非@arg _markAsUsed被设置为false。
 	std::ostream& sout(bool _markAsUsed = true);
 
 	/// Returns the stream that should receive error output. Sets m_hasOutput to true if the
 	/// stream has ever been used unless @arg _markAsUsed is set to false.
+	//返回应该接收错误输出的流。设置m_hasOutput为true，如果流曾经被使用过，除非@arg _markAsUsed被设置为false。
 	std::ostream& serr(bool _markAsUsed = true);
 
 	std::istream& m_sin;
